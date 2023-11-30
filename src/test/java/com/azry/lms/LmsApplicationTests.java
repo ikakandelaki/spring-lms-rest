7aa06143e5b8051a98c2da38026e2eaee38a21a5
ikakandelaki <ikakandelaki777@gmail.com>
2023-11-30 13:44:30 +0400

    init spring boot project

---
+++ src/test/java/com/azry/lms/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azry.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
