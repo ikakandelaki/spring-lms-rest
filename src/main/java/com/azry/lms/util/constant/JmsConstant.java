932893add41e793236df14e2a465a163a726c8ce
ikakandelaki <ikakandelaki777@gmail.com>
2023-12-03 11:00:05 +0400

    fix lms constant

--- src/main/java/com/azry/lms/util/constant/JmsConstant.java
+++ src/main/java/com/azry/lms/util/constant/JmsConstant.java
@@ -2,5 +2,5 @@ package com.azry.lms.util.constant;
 
 public interface JmsConstant {
     String LMS_BORROW_QUEUE = "lms.borrow.queue";
-    String LMS_RETURN_QUEUE = "lms.borrow.queue";
+    String LMS_RETURN_QUEUE = "lms.return.queue";
 }
