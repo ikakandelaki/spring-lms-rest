6cc599606bcd20c0a744c7c0f1eaf639b5fa3938
ikakandelaki <ikakandelaki777@gmail.com>
2023-12-04 01:12:21 +0400

    add documentation with swagger

--- src/main/java/com/azry/lms/controller/BookController.java
+++ src/main/java/com/azry/lms/controller/BookController.java
@@ -3,6 +3,9 @@ package com.azry.lms.controller;
 import com.azry.lms.dto.request.BookRequest;
 import com.azry.lms.dto.response.BookResponse;
 import com.azry.lms.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,6 +24,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
+@Tag(name = "Rest APIs for Book resource")
 public class BookController {
     private final BookService bookService;
 
@@ -29,6 +33,7 @@ public class BookController {
     }
 
     @GetMapping
+    @Operation(summary = "Search books")
     public List<BookResponse> searchBooks(@RequestParam(required = false, defaultValue = "") String query) {
         return bookService.searchBooks(query);
     }
@@ -36,12 +41,16 @@ public class BookController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @Operation(summary = "Add book", description = "Only users with admin role have access to add book")
     public BookResponse addBook(@RequestBody @Valid BookRequest request) {
         return bookService.addBook(request);
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @Operation(summary = "Edit book", description = "Only users with admin role have access to edit book")
     public BookResponse editBook(@PathVariable Long id, @RequestBody @Valid BookRequest request) {
         return bookService.editBook(id, request);
     }
@@ -49,16 +58,22 @@ public class BookController {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @Operation(summary = "Delete book", description = "Only users with admin role have access to delete book")
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
     }
 
     @PutMapping("/{id}/borrow")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @Operation(summary = "Borrow book")
     public BookResponse borrowBook(@PathVariable Long id) {
         return bookService.borrowBook(id);
     }
 
     @PutMapping("/{id}/return")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @Operation(summary = "Return book")
     public BookResponse returnBook(@PathVariable Long id) {
         return bookService.returnBook(id);
     }
--- src/main/java/com/azry/lms/controller/UserController.java
+++ src/main/java/com/azry/lms/controller/UserController.java
@@ -4,6 +4,9 @@ import com.azry.lms.dto.request.UserRequest;
 import com.azry.lms.dto.response.BookResponse;
 import com.azry.lms.dto.response.UserResponse;
 import com.azry.lms.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,6 +24,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @PreAuthorize("hasRole('ROLE_ADMIN')")
+@SecurityRequirement(name = "Bearer Authentication")
+@Tag(name = "Rest APIs for User resource", description = "Only users with admin role have access to these endpoints")
 public class UserController {
     private final UserService userService;
 
@@ -29,23 +34,27 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all users")
     public List<UserResponse> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}/books")
+    @Operation(summary = "Get borrowed books of user")
     public List<BookResponse> getBorrowedBooksOfUser(@PathVariable Long id) {
         return userService.getBorrowedBooksOfUser(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create user")
     public UserResponse createUser(@RequestBody @Valid UserRequest request) {
         return userService.createUser(request);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete user")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
